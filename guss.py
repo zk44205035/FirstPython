681f8f46fae89c3974dd3b59a74b5626083b245f
zk44205035 <44205035@qq.com>
2018-06-07 00:20:57 +0800

    猜测次数统计

--- guss.py
+++ guss.py
@@ -1,13 +1,25 @@
 import random
 running= True
 number=random.randint(0,100)
+
+count = 0
+
 while running:
 	guess = raw_input("Guess the Number:")
 	guess_int = int(guess)
 	if number > guess_int:
+		count+=1
 		print "Too low!"
+		print "Sum:"
+		print count
 	if number < guess_int:
+		count+=1
 		print "Too high!"
+		print "Sum:"
+		print count
 	if number == guess_int:
-		print "You got it!"
+		count+=1
+		print "You got it!" 
+		print "Sum:"
+		print count
 	        running = False
